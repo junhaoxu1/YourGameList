d1294961e1c4f25ccd5c5edd60c4976b8e6b637c
Andreas X <97689187+junhaoxu1@users.noreply.github.com>
2024-01-02 11:51:55 +0100

    Fixed hook to not only show currentUser in review

--- examen/src/hooks/useGetReviews.ts
+++ examen/src/hooks/useGetReviews.ts
@@ -2,10 +2,8 @@ import { where } from 'firebase/firestore'
 import { reviewsCol } from '../services/firebase'
 import { Review } from '../types/Review.types'
 import useStreamCollection from './useStreamCollection'
-import useAuth from './useAuth'
 
 const useGetReviews = (gameId: number) => {
-    const { userEmail, userName, userPhotoUrl } = useAuth();
 
     const { data: reviewsData} = useStreamCollection<Review>(
         reviewsCol,
@@ -16,9 +14,6 @@ const useGetReviews = (gameId: number) => {
 
     const reviewsWithUser = reviews.map(review => ({
         ...review,
-        userName: userName,
-        userEmail: userEmail,
-        userPhotoUrl: userPhotoUrl,
     }));
     return {data: reviewsWithUser};
 }
