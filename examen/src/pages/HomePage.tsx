e841023f1fc79ae5e014e770c7c89d6b97db5969
Andreas X <97689187+junhaoxu1@users.noreply.github.com>
2024-01-11 13:36:16 +0100

    Added small changes to homepage

--- examen/src/pages/HomePage.tsx
+++ examen/src/pages/HomePage.tsx
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
 const HomePage = () => {
+
+  const { currentUser } = useAuth()
+
   return (
-    <div>Welcome To YourGameList!</div>
+    <>
+    <h1>Welcome To YourGameList!</h1>
+    {currentUser ? (
+      <>
+      <h2>Hello {currentUser.email}</h2>
+      <h3>Get started! Click <Link className="border border-light" to="/games">HERE</Link> to browse games</h3>
+      </>
+    ): (
+      <h2>Please <Link className="border border-light" to="/login">Login</Link> or <Link className="border border-light" to="/signup">Sign up</Link> for a better experience!</h2>
+    )}
+    </>
+
   )
 }
 
